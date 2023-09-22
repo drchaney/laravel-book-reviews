b26341a63419b5d796f282ba31c6c676cd032b1a
David Chaney <david.ray.chaney+github@gmail.com>
2023-09-22 13:22:51 -0500

    housekeeping: remove unused methods

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -41,22 +41,6 @@ class BookController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
      * Display the specified resource.
      */
     public function show(int $id)
@@ -76,27 +60,4 @@ class BookController extends Controller
         return view('books.show', ['book' => $book]);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
-    }
 }
